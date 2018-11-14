b8ace8b546085b75d7d09d0ee3e652e6e9204c96
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-14 21:55:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,4 +16,4 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elseif phrase == "Hi Nana, how are you?"
-    puts ""
+    puts "HUH?! SPEAK UP, SONNY!"
