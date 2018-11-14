36b0db68e79b4229c67c34197ec8a2a4f9ab55fb
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-14 22:01:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -23,5 +23,7 @@ def speak_to_grandma(phrase)
     puts "HUH?! SPEAK UP, SONNY!"
     elsif
     phrase == "WHAT DID YOU EAT TODAY?"
-    puts ""
+    puts "NO, NOT SINCE 1938!"
+    esleif
+    phrase == 
     
